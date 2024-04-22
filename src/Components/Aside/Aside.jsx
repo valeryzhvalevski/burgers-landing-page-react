0da6edf9bfd58ff73c59a84241eb6827dd75270e
valeryzhvalevski <valery.zhvalevski@gmail.com>
2024-04-23 00:41:31 +0300

    start aside

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -1,10 +1,17 @@
 import Button from "../Button/Button";
 import style from "./Aside.module.scss";
+import AsideItem from "../AsideItem/AsideItem";
+import data from "../../data/asideData.json";
+import addAsideImg from "../../common/addAsideImg";
 
 export default function Aside() {
+  const newData = addAsideImg(data);
+
   return (
     <div className={style.container}>
-      <Button content="Оформить заказ" color="#FF7020" colorText="white" />
+      {newData.map((item, index) => (
+        <AsideItem image={item.image} title={item.title} price={item.price} weight={item.weight} value={item.value}/>
+      ))}
     </div>
   );
 }
---
+++ src/Components/AsideItem/AsideItem.jsx
@@ -0,0 +1,21 @@
+import style from "./AsideItem.module.scss";
+
+export default function Aside({ image, title , weight, price, value}) {
+  return (
+    <div className={style.containerAsideItem}>
+      <div className={style.product}>
+        <div className={style.wrapperImg}>
+          <img src={image} alt="product" />
+          <p>{title}</p>
+          <p>{weight}</p>
+          <p>{price}</p>
+        </div>
+      </div>
+      <div className={style.counter}>
+        <button className={style.lessBnt}>-</button>
+        <p className={style.valueCounter}>{value}</p>
+        <button className={style.moreBnt}>+</button>
+      </div>
+    </div>
+  );
+}
