58047068aa9b2377510d8f521fe147cb86ce8125
valeryzhvalevski <valery.zhvalevski@gmail.com>
2024-04-23 01:33:56 +0300

    add aside

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -3,15 +3,37 @@ import style from "./Aside.module.scss";
 import AsideItem from "../AsideItem/AsideItem";
 import data from "../../data/asideData.json";
 import addAsideImg from "../../common/addAsideImg";
+import delivery from "../../assets/delivery.png";
 
 export default function Aside() {
   const newData = addAsideImg(data);
 
   return (
     <div className={style.container}>
-      {newData.map((item, index) => (
-        <AsideItem image={item.image} title={item.title} price={item.price} weight={item.weight} value={item.value}/>
-      ))}
+      <div className={style.titleContainer}>
+        <h3>Корзина</h3>
+        <p>4</p>
+      </div>
+      <div className={style.containerContent}>
+        {newData.map((item, index) => (
+          <AsideItem
+            image={item.image}
+            title={item.title}
+            price={item.price}
+            weight={item.weight}
+            value={item.value}
+          />
+        ))}
+      </div>
+      <div className={style.total}>
+        <p>Итого</p>
+        <p>1279₽</p>
+      </div>
+      <Button content="Оформить заказ" color="#FF7020" colorText="white" />
+      <div className={style.delivery}>
+        <img src={delivery} alt="delivery" />
+        <p>Бесплатная доставка</p>
+      </div>
     </div>
   );
 }
--- src/Components/AsideItem/AsideItem.jsx
+++ src/Components/AsideItem/AsideItem.jsx
@@ -1,11 +1,13 @@
 import style from "./AsideItem.module.scss";
 
-export default function Aside({ image, title , weight, price, value}) {
+export default function Aside({ image, title, weight, price, value }) {
   return (
     <div className={style.containerAsideItem}>
       <div className={style.product}>
         <div className={style.wrapperImg}>
           <img src={image} alt="product" />
+        </div>
+        <div className={style.wrapperText}>
           <p>{title}</p>
           <p>{weight}</p>
           <p>{price}</p>
