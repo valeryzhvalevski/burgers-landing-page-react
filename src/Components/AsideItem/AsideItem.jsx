65f1e566e5e395ccf22004ae06c362cc4829701e
valeryzhvalevski <valery.zhvalevski@gmail.com>
2024-04-23 22:08:50 +0300

    add counter aside item

--- src/Components/AsideItem/AsideItem.jsx
+++ src/Components/AsideItem/AsideItem.jsx
@@ -1,6 +1,18 @@
+import React, { useState } from "react";
 import style from "./AsideItem.module.scss";
 
-export default function Aside({ image, title, weight, price, value }) {
+export default function Aside({ image, title, weight, price }) {
+  const [value, setValue] = useState(1); 
+  const decrement = () => {
+    if (value > 1) {
+      setValue(prevValue => prevValue - 1); 
+    }
+  };
+
+  const increment = () => {
+    setValue(prevValue => prevValue + 1); 
+  };
+
   return (
     <div className={style.containerAsideItem}>
       <div className={style.product}>
@@ -14,10 +26,13 @@ export default function Aside({ image, title, weight, price, value }) {
         </div>
       </div>
       <div className={style.counter}>
-        <button className={style.lessBnt}>-</button>
+        <button className={style.lessBnt} onClick={decrement}>-</button>
         <p className={style.valueCounter}>{value}</p>
-        <button className={style.moreBnt}>+</button>
+        <button className={style.moreBnt} onClick={increment}>+</button>
       </div>
     </div>
   );
 }
+
+
+
